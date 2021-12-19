7bb596af2af45c6def2fc8f4172099872b0b4eda
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-18 21:28:58 -0500

    Post init

--- RoLink/frontend/src/pages/register.js
+++ RoLink/frontend/src/pages/register.js
@@ -1,40 +1,127 @@
 import axios from "axios";
 import { useState, useEffect } from 'react';
 
+import validator from 'validator'
+
 import "../css/register.css"
 
+let passwordValidator = require('password-validator');
+
+let schema =  new passwordValidator();
+
+// Add properties to it
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+// .has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+
 export default function Register(Props) {
+    
+    // States
+    const [validEmail, setValidEmail] = useState(true)
+    const [matching, setMatching] = useState(true)
+    const [validPassword, setValidPassword] = useState(true)
+    const [canSubmit, setCanSubmit] = useState(false)
+    const [inputs, setInputs] = useState({ 
+        first: "", 
+        last: "",
+        email: "",
+        password: "",
+        confirm: ""
+    })
+
+    // Functions
+    const validateEmail = (e) => {
+        var email = e.target.value
+      
+        if (validator.isEmail(email)) {
+            setValidEmail(true)
+        } else {
+            setValidEmail(false)
+        }
+        setCanSubmit(validEmail)
+      }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(canSubmit, inputs)
+
+        if (canSubmit) {
+            axios.post("/api/users", inputs) // 4
+                .then(res => alert("Form Submitted")) // 5
+                .catch(errors => console.log(errors)) // 6
+        }
+    }
+
+    const handleChange = (e) => {
+        const value = e.target.value
+        const name = e.target.name
+        const copy = Object.assign({}, inputs)
+
+        copy[name] = value
+
+        if (value === "") {
+            setCanSubmit(false)
+        } else {
+            setCanSubmit(true)
+        }
+
+        if (name === "confirm") {
+            let bool = copy[name] === copy['password']
+            setCanSubmit(bool)
+            setMatching(bool)
+        } else if (name === "email") {
+            validateEmail(e)
+            
+        } else if (name === "password") {
+            setValidPassword(schema.validate(value))
+            setCanSubmit(validPassword)
+        }
+
+        
+
+        setInputs(copy)
+    }
+
+    console.log(canSubmit)
 
     return (
         <div className="background">
-            <div class="signup-box">
+            <div className="signup-box">
                         <h1>Register</h1>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className="Name">
                                 <div className="Field">
-                                    <input type="text" placeholder="First Name" />
+                                    <input name="first" onChange={handleChange} type="text" placeholder="First Name" value={inputs.first || ""} />
                                 </div>
                                 <div className="Field">
-                                    <input type="text" placeholder="Last Name" />
+                                    <input name="last" onChange={handleChange} type="text" placeholder="Last Name" value={inputs.last} />
                                 </div>
                             </div>
                             
                             <div className="Field">
-                                <input type="email" placeholder="Email" />
+                                {validEmail ? '' : <p>Invalid Valid</p>}
+                                <input name="email"  onChange={handleChange} type="email" placeholder="Email" value={inputs.email} />
                             </div>
                             
                             
                             <div className="Field">
-                                <input type="password" placeholder="Password" />
+                                {validPassword ? '' : <p>Invalid Password</p>}
+                                <input name="password" onChange={handleChange} type="password" placeholder="Password" value={inputs.password} />
                             </div>
 
                             <div className="Field">
-                                <input type="password" placeholder="Confirm" />
+                                {matching ? '' : <p>Does not match password</p>}
+                                <input name="confirm" onChange={handleChange} type="password" placeholder="Confirm" value={inputs.confirm} />
                             </div>
-
-                            <input className="Submit" type="button" value="Submit" />
+                            <input className="Submit" type="submit" value="Submit" />
                         </form>
                     </div>
         </div>
-      );
+    );
 }
