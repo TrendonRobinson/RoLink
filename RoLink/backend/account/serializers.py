effc2c31acb19c8e52c369638c8b38f3ef643684
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-19 12:35:05 -0500

    serializer changes

--- RoLink/backend/account/serializers.py
+++ RoLink/backend/account/serializers.py
@@ -7,12 +7,12 @@ from rest_framework.authtoken.models import Token
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['id', 'email', 'username', 'first', 'last',
+        fields = ['id', 'email', 'username', 'password', 'first', 'last',
                   'date_joined', 'last_login', 'is_admin', 'is_staff']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
-    # def create(self, validated_data):
-    #     print(self, validated_data)
-    #     user = Account.objects.create_user(**validated_data)
-    #     Token.objects.create(user=user)
-    #     return user
+    def create(self, validated_data):
+        print(self, validated_data)
+        user = Account.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
